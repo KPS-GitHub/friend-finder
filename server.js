dc25e62d0f7d903f9cf620c2e209e5f0bb1d910e
Kennan <kennan.smith1@gmail.com>
2018-06-05 10:57:59 -0600

    filled out server.js

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+// dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+// express configuration
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// router
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
