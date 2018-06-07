d97c1183993a2f9181f0bc7be5c117e88fe8b28a
Kennan Smith <kennan.smith1@gmail.com>
2018-06-06 20:38:21 -0600

    minor tweaks - need to fill out api post route functionality - possible bug: navigating to /api/friends/ route in browser only returns {} rather than the full json object

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,19 +1,14 @@
 // point to friends.js (data file)
-var friendData = require("../data/friends");
+var friendData = require("../data/friends.js");
 
 
 module.exports = function(app) {
 
-<<<<<<< HEAD
     // A GET route which will display a JSON of all possible friends. 
-=======
-    // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends. 
->>>>>>> 77493281e5513de59d31daff32fe2f27dfc6a099
     app.get("/api/friends", function(req, res) {
-        res.json(friendData);
+        res.json(req.body);
     });
 
-<<<<<<< HEAD
     // a POST route which will handle the functionality of updating the server with the user data, comparing it against the friendsData, and choosing the best match
     app.post("/api/friends", function(req, res) {
         
@@ -26,12 +21,6 @@ module.exports = function(app) {
 
         // 
 
-=======
-    // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-    app.post("/api/friends", function(req, res) {
-        friendData.push(req.body);
-        res.json(true);
->>>>>>> 77493281e5513de59d31daff32fe2f27dfc6a099
     });
     
 }
